65a8aeb25d9f7901d92009cc553b451423a5ed1a
MichaelBWilliams <michaelbevins.williams@gmail.com>
2017-05-05 17:39:34 -0400

    Home page updated and stylized

--- src/wmi/settings.py
+++ src/wmi/settings.py
@@ -190,12 +190,12 @@ AWS_ACCESS_KEY_ID = access_key
 AWS_SECRET_ACCESS_KEY = secret_key
 AWS_STORAGE_BUCKET_NAME = 'web-map-interface'
 
-STATICFILES_STORAGE = 'storages.backends.s3boto3.S3Boto3Storage' #static files such as css.
+#STATICFILES_STORAGE = 'storages.backends.s3boto3.S3Boto3Storage' #static files such as css.
 DEFAULT_FILE_STORAGE = 'storages.backends.s3boto3.S3Boto3Storage' #media uploads
 S3_URL = '//%s.s3.amazonaws.com/' %AWS_STORAGE_BUCKET_NAME
 MEDIA_URL = S3_URL + "media/"
-STATIC_URL = S3_URL + "static/"
-STATIC_ROOT = os.path.join(BASE_DIR, "static/")
+#STATIC_URL = S3_URL + "static/"
+#STATIC_ROOT = os.path.join(BASE_DIR, "static/")
 
 
 
