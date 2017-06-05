918c8a8be86a90715f25abed66f9e1ce8f9276e9
MichaelBWilliams <michaelbevins.williams@gmail.com>
2017-06-05 11:38:19 -0400

    updated views and html for each country and mapsets

--- src/maps/views.py
+++ src/maps/views.py
@@ -40,71 +40,99 @@ class ResourcesView(LoginRequiredMixin, View):
 class kenya_view(LoginRequiredMixin, GroupRequiredMixin, s3resource, View):
   group_required = ["KE", "VFI", "AFR"]
   raise_exception = True
-  #mapmatrix = "mapmatrixFAKE"
   def get(self, request, *args, **kwargs):
     COUNTRY = 'kenya'
     country_list = s3resource.country_filter(self, COUNTRY)
     return render(request, "maps/kenya.html", {'country_list' : country_list})
   
-class malawi_view(LoginRequiredMixin, GroupRequiredMixin, View):
+class kenya_view(LoginRequiredMixin, GroupRequiredMixin, s3resource, View):
+  group_required = ["KE", "VFI", "AFR"]
+  raise_exception = True
+  def get(self, request, *args, **kwargs):
+    COUNTRY = 'kenya'
+    country_list = s3resource.country_filter(self, COUNTRY)
+    return render(request, "maps/kenya.html", {'country_list' : country_list})
+  
+class malawi_view(LoginRequiredMixin, GroupRequiredMixin, s3resource, View):
   group_required = ["MW", "VFI", "AFR"]
   raise_exception = True
   def get(self, request, *args, **kwargs):
-    return render(request, "maps/malawi.html", {})
+    COUNTRY = 'malawi'
+    country_list = s3resource.country_filter(self, COUNTRY)
+    return render(request, "maps/malawi.html", {'country_list' : country_list})
   
-class tanzania_view(LoginRequiredMixin, GroupRequiredMixin, View):
+class tanzania_view(LoginRequiredMixin, GroupRequiredMixin, s3resource, View):
   group_required = ["TZ", "VFI", "AFR"]
   raise_exception = True
   def get(self, request, *args, **kwargs):
-    return render(request, "maps/tanzania.html", {})
+    COUNTRY = 'tanzania'
+    country_list = s3resource.country_filter(self, COUNTRY)
+    return render(request, "maps/tanzania.html", {'country_list' : country_list})
   
-class uganda_view(LoginRequiredMixin, GroupRequiredMixin, View):
+class uganda_view(LoginRequiredMixin, GroupRequiredMixin, s3resource, View):
   group_required = ["UG", "VFI", "AFR"]
   raise_exception = True
   def get(self, request, *args, **kwargs):
-    return render(request, "maps/uganda.html", {})
-  
-class zambia_view(LoginRequiredMixin, GroupRequiredMixin, View):
+    COUNTRY = 'uganda'
+    country_list = s3resource.country_filter(self, COUNTRY)
+    return render(request, "maps/uganda.html", {'country_list' : country_list})
+
+class zambia_view(LoginRequiredMixin, GroupRequiredMixin, s3resource, View):
   group_required = ["ZM", "VFI", "AFR"]
   raise_exception = True
   def get(self, request, *args, **kwargs):
-    return render(request, "maps/zambia.html", {})
+    COUNTRY = 'zambia'
+    country_list = s3resource.country_filter(self, COUNTRY)
+    return render(request, "maps/zambia.html", {'country_list' : country_list})
   
-class cambodia_view(LoginRequiredMixin, GroupRequiredMixin, View):
+class cambodia_view(LoginRequiredMixin, GroupRequiredMixin, s3resource, View):
   group_required = ["KH", "VFI", "ASR"]
   raise_exception = True
   def get(self, request, *args, **kwargs):
-    return render(request, "maps/cambodia.html", {})
+    COUNTRY = 'cambodia'
+    country_list = s3resource.country_filter(self, COUNTRY)
+    return render(request, "maps/cambodia.html", {'country_list' : country_list})
   
-class myanmar_view(LoginRequiredMixin, GroupRequiredMixin, View):
+class myanmar_view(LoginRequiredMixin, GroupRequiredMixin, s3resource, View):
   group_required = ["MM", "VFI", "ASR"]
   raise_exception = True
   def get(self, request, *args, **kwargs):
-    return render(request, "maps/myanmar.html", {})
+    COUNTRY = 'myanmar'
+    country_list = s3resource.country_filter(self, COUNTRY)
+    return render(request, "maps/myanmar.html", {'country_list' : country_list})
   
-class philippines_view(LoginRequiredMixin, GroupRequiredMixin, View):
+class philippines_view(LoginRequiredMixin, GroupRequiredMixin, s3resource, View):
   group_required = ["PH", "VFI", "ASR"]
   raise_exception = True
   def get(self, request, *args, **kwargs):
-    return render(request, "maps/philippines.html", {})
+    COUNTRY = 'philippines'
+    country_list = s3resource.country_filter(self, COUNTRY)
+    return render(request, "maps/philippines.html", {'country_list' : country_list})
   
-class srilanka_view(LoginRequiredMixin, GroupRequiredMixin, View):
+class srilanka_view(LoginRequiredMixin, GroupRequiredMixin, s3resource, View):
   group_required = ["LK", "VFI", "ASR"]
   raise_exception = True
   def get(self, request, *args, **kwargs):
-    return render(request, "maps/srilanka.html", {})
+    COUNTRY = 'srilanka'
+    country_list = s3resource.country_filter(self, COUNTRY)
+    return render(request, "maps/srilanka.html", {'country_list' : country_list})
   
-class ecuador_view(LoginRequiredMixin, GroupRequiredMixin, View):
+class ecuador_view(LoginRequiredMixin, GroupRequiredMixin, s3resource, View):
   group_required = ["EC", "VFI", "LAR"]
   raise_exception = True
   def get(self, request, *args, **kwargs):
-    return render(request, "maps/ecuador.html", {})
+    COUNTRY = 'ecuador'
+    country_list = s3resource.country_filter(self, COUNTRY)
+    return render(request, "maps/ecuador.html", {'country_list' : country_list})
   
-class honduras_view(LoginRequiredMixin, GroupRequiredMixin, View):
+class honduras_view(LoginRequiredMixin, GroupRequiredMixin, s3resource, View):
   group_required = ["HN", "VFI", "LAR"]
   raise_exception = True
   def get(self, request, *args, **kwargs):
-    return render(request, "maps/honduras.html", {})
+    COUNTRY = 'srilanka'
+    country_list = s3resource.country_filter(self, COUNTRY)
+    return render(request, "maps/srilanka.html", {'country_list' : country_list})
+  
 # End of country/MFI specific views
 
 class test(LoginRequiredMixin, s3resource, View):
